6a5555ce9489676c39db1c37cba44a6db0a70539
Christian <CStochholm@kami-io.com>
2021-10-07 20:36:15 +0200

    Cleaned up work station
    Renamed SpawnIngredient to SpawnItem
    Added comments to SpawnItem
    Removed unused code
    Added ingredient and product tag
    Cleaned up test ingredient

--- Assets/Scripts/SpawnItem.cs
+++ Assets/Scripts/SpawnItem.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnIngredient : MonoBehaviour
+public class SpawnItem : MonoBehaviour
 {
+
+    // Variables
+
     public GameObject myPrefab;
-    Rigidbody rb;
     
+    
+    // Start
 
     private void Start()
     {
       
     }
 
+    // Spawn prefab
+
     public void Spawn(GameObject Ingredient)
     {
+
         Instantiate(myPrefab, transform.position + new Vector3(0,1,0), transform.rotation);
-        rb = myPrefab.gameObject.GetComponent<Rigidbody>();
-        rb.AddForce(transform.up * 1000, ForceMode.Impulse);
+        
     }
 
     
