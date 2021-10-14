758e744113a80fa42750d91ac7ad48d12c3bb9d4
Emil Bülow <emil9107@gmail.com>
2021-10-14 17:02:42 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Mixer.cs
+++ Assets/Scripts/Mixer.cs
@@ -4,27 +4,24 @@ using UnityEngine;
 
 public class Mixer : ActivateWorkstation
 {   
-   
+    
     public List<GameObject> Ingredienser;
-
     public List<GameObject> Opskrifter;
 
     
     public bool HarTingIHaanden;
 
     private bool Blander;
-
-    private void Awake()
+    
+    public override void Activate()
     {
         if (Blander == false && HarTingIHaanden == true)
         {
+            //tilføj ingrediens til listen
             
-
-
         } else if (Blander == false && HarTingIHaanden == false)
         {
-
+            //start mixer minigame
         }
-        
     }
 }
